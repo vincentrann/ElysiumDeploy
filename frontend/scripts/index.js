085c9594411b1f1b37eec0296d8354620d74cb9d
mayowaibi <isaac.ibidun@gmail.com>
2024-11-20 07:21:22 -0500

    Completed catalog/landing page

---
+++ frontend/scripts/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * add active class on header when scrolled 200px from top
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 200 ? header.classList.add("active")
+    : header.classList.remove("active");
+})
