073ed66fb7fc4c6442add8696c39d9b334e1c4b5
Vincent Tran <vinc3ntran@gmail.com>
2024-12-19 14:44:54 -0500

    Update CORSConfig.java

--- src/main/java/org/elysium/backend/CORSConfig.java
+++ src/main/java/org/elysium/backend/CORSConfig.java
@@ -15,7 +15,7 @@ public class CORSConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:5500", "http://127.0.0.1:5500", "https://elysiumdeploy-production.up.railway.app")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
